f91827018362bd22611b2708ec60161476318333
mvrsc <mvareschi@outlook.com>
2024-06-13 23:22:21 +0200

    Add files via upload

---
+++ VR5043167_VR503119/main.s
@@ -0,0 +1,112 @@
+.section .data
+
+inizio:
+	.string "Inserisci 1 per EDF, 2 per HPF, 3 per uscire.\n"
+inizio_len:
+	.long . -inizio
+	
+err_input:
+	.string "hai inserito un carattere non valido\n"
+err_input_len:
+	.long . -err_input
+	
+err_param:
+	.string "Errore! Questo programma richiede esattamente un file di testo come parametro\n"
+err_param_len:
+	.long . -err_param
+
+.section .bss
+input:
+	.string ""
+
+.section .text
+	.global _start
+
+_start:
+	####################
+	#CONTROLLA VALIDITA' PARAMETRO
+	movl (%esp), %ebx
+	cmpl $2, %ebx
+	jne param_error
+	
+	
+	####################
+	#LEGGI PARAMETRO
+
+	movl %esp, %ecx #metti l'indirizzo dello stack in ecx
+	addl $8, %ecx #scorri indietro di 2 posizioni
+	movl (%ecx), %ebx #l'indirizzo del file è ora in ebx
+	
+	call read_push
+	cmpl $0, %edi #esco se la funzione mi ha restituito un errore
+	je EXIT
+	
+selezione:	
+	########################
+	#SCRIVI MESSAGGIO
+	
+	movl $4, %eax # 4 = syscall WRITE
+	movl $1, %ebx # 1 = write to standard output
+	leal inizio, %ecx # metti il messaggio in ECX
+	movl inizio_len, %edx #lunghezza del messaggio in EDX
+	int  $0x80
+
+	########################
+	#LEGGI INPUT UTENTE
+	movl $3, %eax # 3 = syscall READ
+	movl $0, %ebx # 0 = read from keyboard
+	leal input, %ecx # metti quello che leggi in input
+	movl $1, %edx #leggi un carattere solo
+	int  $0x80
+	
+
+	########################
+	#SELEZIONE
+	cmpb $49, input
+		je sel_EDF
+	cmpb $50, input
+		je sel_HPF
+	cmpb $51, input
+		je EXIT
+	
+	jmp input_error
+		
+	
+
+sel_EDF:
+	call edf
+	jmp calcola	
+	
+sel_HPF:	
+	call hpf
+
+calcola:
+	call output
+	
+EXIT:
+	movl $1, %eax # exit(0)
+	movl $0, %ebx
+	int  $0x80
+
+
+	#######################
+	#TORNA AL MENU SE LA SELEZIONE NON E' VALIDA
+input_error:
+	movl $4, %eax # 4 = syscall WRITE
+	movl $1, %ebx # 1 = write to standard output
+	leal err_input, %ecx # metti il messaggio in ECX
+	movl err_input_len, %edx #lunghezza del messaggio in EDX
+	int  $0x80
+	jmp selezione
+	
+	
+	#######################
+	#ESCI SE NON HAI UN FILE DA APRIRE
+param_error:
+	movl $4, %eax # 4 = syscall WRITE
+	movl $1, %ebx # 1 = write to standard output
+	leal err_param, %ecx # metti il messaggio in ECX
+	movl err_param_len, %edx #lunghezza del messaggio in EDX
+	int  $0x80
+	jmp EXIT
+
---
+++ VR5043167_VR503119/read_push.s
@@ -0,0 +1,197 @@
+###########################################################################
+
+#funzione che legge il file degli ordini e copia i valori nello stack
+
+
+#		LOOP fino a EOF
+# loop read_char fino a \n
+	# loop read_char fino a ,
+	# leggi stringa e converti in mumero (variabile num)
+	# quando leggi virgola conta quanti campi del prodotto hai gia' letto (0, 1 o 2)
+	# 0 campi letti -> hai appena letto il primo campo (id)
+	# 1 campi letti -> hai appena letto il secondo campo (durata)
+	# 2 campi letti -> hai appena letto il terzo campo (scadenza)
+# quando leggi \n trasferisci i campi letti in AX due alla volta (in AL e in AH) e fai push su stack (x2)
+# quando arrivi a EOF fai push degli ultimi campi letti e chiudi file 
+
+############################################################################
+
+.section .data
+
+err_file: 
+	.string "Errore nell'apertura del file"	
+err_file_len: 
+	.long  . -err_file
+fd:
+	.long 0	
+char:
+	.string ""
+num:	#variabile in cui converto la stringa in numero
+	.long 0
+id:	#variabile in cui sposto num per fare la push
+	.long 0
+dur:	#variabile in cui sposto num per fare la push
+	.long 0
+scad:	#variabile in cui sposto num per fare la push
+	.long 0
+prior:	#variabile in cui sposto num per fare la push
+	.long 0
+conta_c:	#contatore dei campi letti per fare pushw di AX su stack 
+	.long 0
+conta_p:	#conta quanti prodotti ho inserito da passare come valore per il SORTING (conta le push eseguite)
+	.long 0
+
+.section .text			#### cambiare in function
+	.global read_push
+	
+.type read_push, @function
+
+read_push:
+
+	popl %ebp #salvo in edi l'indirizzo dell'istruzione da chiamare a fine funzione
+
+##########################################################
+
+#				APRI FILE	
+
+##########################################################
+
+	#l'indirizzo del file è stato messo in ebx dal chiamante
+	movl $5, %eax #syscall apri file
+
+	xorl %ecx, %ecx #azzero ecx perche' read only
+	int $0x80
+	
+	movl %eax, fd 	#indirizzo file in eax
+		
+	cmp $0, %eax   #in caso di errore apertura esce
+	jle file_error
+	
+
+
+#########################################################
+
+#				LEGGI FILE e PUSH su STACK
+
+#########################################################
+
+readchar_loop:
+	movl $3, %eax #syscall read
+	movl fd, %ebx #leggi da fd
+	movl $char, %ecx #quello che leggi va messo in char
+	movl $1, %edx #leggi un solo carattere
+	int $0x80
+	cmp $0, %eax        # Controllo se ci sono errori o EOF
+		je fine_file     
+
+	xorl %ebx, %ebx
+	movb char, %bl 	#metto in bl il char letto dal file
+	cmp $13, %bl    #lavorando in windows mette sia 13 che 10 alla fine della linea (CRLF)
+		je readchar_loop
+	cmp $10, %bl	# vedo se e' stato letto il carattere '\n'
+		je fine_riga	# se ho letto \n vado alla push
+	cmp $44, %bl    # vedo se e' stato letto il carattere ','
+		je fine_campo	# se ho letto ',' controllo quanti campi ho letto
+
+	
+	subb $48, %bl            # converto il codice ASCII della cifra nel numero corrispondente
+	movl $10, %edx
+	movl num, %eax		#metto in EAX il numero letto finora
+	mulb %dl                # EAX = EAX * 10 (alla prima occorrenza ho EAX=0)
+	addl %ebx, %eax		  # sommo EBX (ultima cifra letta) a EAX (e la somma va in EAX)
+	movl %eax, num		#metto in num il valore aggiornato
+	jmp readchar_loop	#leggo il carattere seguente
+
+fine_campo:
+# se ho letto ',' controllo quanti campi ho letto per capire che variabile valorizzare
+	cmpl $0, conta_c		
+	jne durata
+	incl conta_c	# incremento numero campi lette
+	movl num, %eax
+	movl %eax, id	
+	movl $0, num #azzero num
+	jmp readchar_loop # leggo il carattere successivo (cioe' la campo successiva)
+
+durata:
+	cmpl $1, conta_c
+	jne scadenza
+	incl conta_c # incremento numero campi letti
+	movl num, %eax
+	movl %eax, dur
+	movl $0, num #azzero num
+	jmp readchar_loop
+
+scadenza:
+	movl num, %eax
+	movl %eax, scad
+	movl $0, num #azzero num
+	jmp readchar_loop	
+	
+
+fine_riga:		# push dei valori letti sullo stack
+	
+	movb id, %ah
+ 	movb dur, %al
+	pushw %ax	
+ 	movl num, %eax
+	movl %eax, prior
+	movb scad, %ah
+ 	movb prior, %al
+	pushw %ax	
+	movl $0, conta_c #azzero contatore campi letti
+	movl $0, num #azzero num
+	movl $0, id
+	movl $0, dur
+	movl $0, scad
+	movl $0, prior
+	incl conta_p	#incremento numero prodotti caricati
+	jmp readchar_loop
+
+fine_file:	# push dei valori letti sullo stack e fine read_char loop
+#eventualmente invertire ordine (vedere ordine nel sorting)
+
+
+/* QUESTA PARTE NON SERVE PER FILE DI TESTO FORMATTATI PER LINUX
+	movl num, %eax
+	movl %eax, prior
+	movb id, %ah
+ 	movb dur, %al
+	pushw %ax	
+ 	movl num, %eax
+	movl %eax, prior
+	movb scad, %ah
+ 	movb prior, %al
+	pushw %ax	
+	incl conta_p	#incremento numero prodotti caricati
+*/
+
+
+#############################
+
+#		CHIUDI FILE
+
+#############################
+
+close_file:	
+	movl $6, %eax # SYS CLOSE
+	movl fd, %ebx # chiudi questo file qua
+	int  $0x80
+	
+	movl conta_p, %edi #metto la lunghezza in edi per chiamare l'algoritmo di sorting
+	
+EXIT:
+	pushl %ebp #rimetto in cima allo stack l'indirizzo della funzione da chiamare	
+	ret
+	
+file_error:
+	movl $4, %eax # 4 = syscall WRITE
+	movl $1, %ebx # 1 = write to standard output
+	leal err_file, %ecx # metti il messaggio in ECX
+	movl err_file_len, %edx #lunghezza del messaggio in EDX
+	int  $0x80
+	movl $0, %edi #lo uso come codice di errore per la funzione chiamante
+	
+	jmp EXIT
+
+
+  
